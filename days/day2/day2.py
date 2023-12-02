36144f39eb27e77925a2474ed815da1253b8a8cc
robertpikmets <robertpikmets@gmail.com>
2023-12-02 14:33:20 +0200

    Day 2 part 2

--- days/day2/day2.py
+++ days/day2/day2.py
@@ -1,27 +1,27 @@
-# import time
+import math
 
 with open("days/day2/data.txt") as file:
     games = [line.rstrip() for line in file]
 
-limits = {
+LIMITS = {
     "red": 12,
     "green": 13,
     "blue": 14,
 }
 
-# t0 = time.time()
+# PART 1
 answer = 0
 for game in games:
     possible = True
     sets = game.split(": ")[1].split("; ")
     # avoiding re.finditer to make it more fun
-    for game_set in sets:
-        for colour in limits.keys():
+    for colour in LIMITS.keys():
+        for game_set in sets:
             i = game_set.find(colour)
             if i != -1:
                 # thank god there aren't 3 digit numbers (i.e. >= 100)
                 cube_count = game_set[: i - 1][-2:]
-                if int(cube_count) > limits[colour]:
+                if int(cube_count) > LIMITS[colour]:
                     possible = False
                     break
         if not possible:
@@ -29,6 +29,19 @@ for game in games:
     if possible:
         answer += int(game.split(": ")[0].split(" ")[1])
 print(answer)
-# t1 = time.time()
-# total = t1 - t0
-# print(total)
+
+# PART 2
+answer = 0
+for game in games:
+    max_cubes = {"red": 0, "green": 0, "blue": 0}
+    sets = game.split(": ")[1].split("; ")
+    # avoiding re.finditer to make it more fun
+    for colour in max_cubes.keys():
+        for game_set in sets:
+            i = game_set.find(colour)
+            if i != -1:
+                cube_count = int(game_set[: i - 1][-2:])
+                if cube_count > max_cubes[colour]:
+                    max_cubes[colour] = cube_count
+    answer += math.prod(max_cubes.values())
+print(answer)
